b3ff954d119c25393bf001728d670c1e40ca3f22
Indomite <Indomite@qq.com>
2021-01-18 21:14:40 +0800

    📝

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,7 +2,39 @@
     <el-container class="homeContainer">
       <!-- 侧边栏 -->
         <el-aside width="200px">
-            aside
+          <!-- 菜单栏 -->
+          <el-col :span="12">
+          <div class="logoImg">
+            <img src="../../assets/logo.jpg" alt="">
+          </div>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-menu-item index="1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-col>
         </el-aside>
         <el-container>
           <!-- 头部 -->
@@ -26,6 +58,13 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.logoImg{
+  align-items: center;
+  background-color: transparent;
+  > img {
+    height: 56px;
+  }
+}
 .homeContainer {
   height: 100vh;
 }
