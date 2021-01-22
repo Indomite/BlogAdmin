0a2aa8f8a4b904f076f1c7ee5ce45282c79c9d86
Indomite <Indomite@qq.com>
2021-01-23 00:02:14 +0800

    🐛 修复BUG

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,15 +3,11 @@
       <!-- 侧边栏 -->
         <el-aside :width="isCollapse ? '64px':'200px'">
           <!-- 菜单栏 -->
-          <el-col :span="12">
-          <div class="logoImg">
-            <img src="../../assets/Homyit.png" alt="">
-          </div>
+            <div class="logoImg">
+              <img src="../../assets/Homyit.png" alt="">
+            </div>
           <el-menu
             default-active="2"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
             background-color="#545c64"
             text-color="#fff"
             active-text-color="#ffd04b"
@@ -25,34 +21,27 @@
                 <i class="el-icon-user-solid"></i>
                 <span>用户信息</span>
               </template>
-              <el-menu-item-group>
-                <el-menu-item index="/users">用户列表</el-menu-item>
-              </el-menu-item-group>
+              <el-menu-item index="/users">用户列表</el-menu-item>
             </el-submenu>
             <el-submenu index="/article">
               <template slot="title">
                 <i class="el-icon-document"></i>
                 <span>文章信息</span>
               </template>
-              <el-menu-item-group>
                 <el-menu-item index="/article">文章列表</el-menu-item>
-              </el-menu-item-group>
             </el-submenu>
             <el-submenu index="/cate">
               <template slot="title">
                 <i class="el-icon-menu"></i>
                 <span>分类信息</span>
               </template>
-              <el-menu-item-group>
                 <el-menu-item index="/cate">分类列表</el-menu-item>
-              </el-menu-item-group>
             </el-submenu>
             <el-menu-item index="/setting">
               <i class="el-icon-s-tools"></i>
               <span>网站配置</span>
             </el-menu-item>
           </el-menu>
-        </el-col>
         </el-aside>
         <el-container>
           <!-- 头部 -->
