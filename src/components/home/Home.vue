ff9b06bddae4f9551b27f1a6de2c66d8cfe57661
Indomite <Indomite@qq.com>
2021-01-22 23:17:08 +0800

    📝 router

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -16,42 +16,40 @@
             text-color="#fff"
             active-text-color="#ffd04b">
             <el-menu-item index="1">
-              <i class="el-icon-menu"></i>
+              <i class="el-icon-s-home"></i>
               <span slot="title">首页</span>
             </el-menu-item>
             <el-submenu index="2">
               <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <i class="el-icon-user-solid"></i>
+                <span>用户信息</span>
               </template>
               <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="2-1">选项1</el-menu-item>
-                <el-menu-item index="2-2">选项2</el-menu-item>
+                <el-menu-item index="2-1">用户列表</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="3">
               <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <i class="el-icon-document"></i>
+                <span>文章信息</span>
               </template>
               <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="3-1">选项1</el-menu-item>
-                <el-menu-item index="3-2">选项2</el-menu-item>
+                <el-menu-item index="3-1">文章列表</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-submenu index="4">
               <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <i class="el-icon-menu"></i>
+                <span>分类信息</span>
               </template>
               <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="4-1">选项1</el-menu-item>
-                <el-menu-item index="4-2">选项2</el-menu-item>
+                <el-menu-item index="4-1">分类列表</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
+            <el-menu-item index="5">
+              <i class="el-icon-s-tools"></i>
+              <span>网站配置</span>
+            </el-menu-item>
           </el-menu>
         </el-col>
         </el-aside>
