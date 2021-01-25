a4642e91ecfbd0d56eafd7f5dce7e1c0b2eb52b2
Indomite <Indomite@qq.com>
2021-01-25 14:52:29 +0800

    📝 用户列表完成

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -8,22 +8,42 @@
     <!-- 卡片视图 -->
     <el-card>
       <!-- 输入框 -->
-      <el-row>
-        <el-col>
+      <el-row :gutter="20">
+        <el-col :span="4">
           <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
         </el-col>
+        <el-col :span="8">
+          <el-input
+            v-model="queryInfo.query"
+            placeholder="请输入内容"
+            class="input-with-select"
+            clearable
+            @clear="getUserList">
+            <el-button @click="getUserList" slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
       </el-row>
-      <el-table border stripe>
+      <!-- 用户列表区 -->
+      <el-table :data="userList" border stripe>
         <el-table-column type="index"></el-table-column>
         <el-table-column label="姓名" prop="username"></el-table-column>
-        <el-table-column label="邮箱" prop="emial"></el-table-column>
-        <el-table-column label="权限" prop="role"></el-table-column>
-        <el-table-column label="状态" prop="status"></el-table-column>
-        <el-table-column label="操作" width="300px">
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="权限" prop="role_id">
+          <template slot-scope="scope">
+            <el-tag type="warning" v-if="scope.row.role_id === 20">管理员</el-tag>
+            <el-tag type="success" v-else-if="scope.row.role_id === 10">用户</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="状态" prop="status">
+          <template slot-scope="scope">
+            <el-tag type="success" v-if="scope.row.status === '1'">未锁定</el-tag>
+            <el-tag type="danger" v-else-if="scope.row.status === '0'">锁定</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="240px">
           <template>
             <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
             <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
-            <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -31,11 +51,11 @@
       <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page="currentPage4"
-        :page-sizes="[5, 10, 20, 50]"
-        :page-size="100"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 10, 20, 50]"
+        :page-size="queryInfo.pagesize"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="100">
+        :total="total">
       </el-pagination>
       <!-- 对话框 -->
       <el-dialog
@@ -69,14 +89,85 @@
 <script>
 export default {
   data () {
-    return {
-      addDialogVisible: false
+    var checkEmail = (rule, value, cb) => {
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        return cb()
+      }
+      cb(new Error('请输入正确的邮箱'))
     }
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 1
+      },
+      userList: [],
+      total: 0,
+      addDialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 5, max: 10, message: '长度在5-10个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在6-15个字符', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  created () {
+    this.getUserList()
   },
   methods: {
     // 监听添加用户对话框的关闭事件
     addDialogClosed () {
       this.$refs.addFormRef.resetFields()
+    },
+    // 获取用户信息
+    async getUserList () {
+      const { data: res } = await this.$http.get('user', { params: this.queryInfo })
+      if (res.code !== 200) return this.$message.error('用户列表获取失败')
+      this.userList = res.data
+      this.total = res.data.length
+      // console.log(res.data)
+    },
+    // 监听页码的变化
+    handleSizeChange (newSize) {
+      console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    // 监听页码值的变化
+    handleCurrentChange (newPage) {
+      console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
+    },
+    // 添加用户对话框
+    addUser () {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('users', this.addForm)
+        if (res.meta.status !== 201) {
+          this.$message.error('添加用户失败')
+        }
+        this.$message.success('添加用户成功')
+        this.addDialogVisible = false
+        this.getUserList()
+        // console.log(valid)
+      })
     }
   }
 }
